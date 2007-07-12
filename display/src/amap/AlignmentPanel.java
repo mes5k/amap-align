102cb1f2ce22022fae1ec08cbfdf902a3f5430a5
mike.smoot <mike.smoot@f0c3ff80-3a33-0410-b2d5-39b5948eb0c4>
2007-07-12 05:29:28 +0000

    added constant spacing and background colors

--- display/src/amap/AlignmentPanel.java
+++ display/src/amap/AlignmentPanel.java
@@ -33,6 +33,9 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.geom.Point2D;
+import java.awt.font.GlyphVector;
+import java.awt.font.FontRenderContext;
 
 public class AlignmentPanel extends JPanel implements ComponentListener {
 
@@ -41,11 +44,15 @@ public class AlignmentPanel extends JPanel implements ComponentListener {
 	int index;
 	int nameXOff; 
 	int nameYOff;
+	int lineWidth;
+	int glyphWidth;
 	int len; 
 	int groupOff;
 	Font defaultFont;
 	int height;
 	int width;
+	int os;
+	Map<String,Color> colMap;
 	public AlignmentPanel(List<Alignment> aligns, int initWidth) {
 		super();
 		this.aligns = aligns;
@@ -55,6 +62,22 @@ public class AlignmentPanel extends JPanel implements ComponentListener {
 		Collections.sort(orderedKeys);
 		defaultFont = new Font("Courier",Font.PLAIN,12);
 		index = 0;
+		// Aaaaargh
+		if ( System.getProperty("os.name").matches(".*[Mm][Aa][Cc].*") )
+			os = -1;
+		else
+			os = 1;
+		colMap = new HashMap<String,Color>();
+		colMap.put("0",new Color(255,255,255)); 
+		colMap.put("1",new Color(0,128,255)); // CMYK 1, 0.5, 0, 0
+		colMap.put("2",new Color(64,25,255)); // CMYK 0.75, 0.9, 0, 0
+		colMap.put("3",new Color(255,0,255)); // CMYK 0, 1, 0, 0
+		/*
+		colMap.put("0",new Color(0,128,255)); // CMYK 1, 0.5, 0, 0
+		colMap.put("1",new Color(255,230,41)); // CMYK 0, 0.1, 0.84, 0
+		colMap.put("2",new Color(64,25,255)); // CMYK 0.75, 0.9, 0, 0
+		colMap.put("3",new Color(255,0,255)); // CMYK 0, 1, 0, 0
+		*/
 		sizeScreen( new Dimension(initWidth,0) );
 	}
 
@@ -63,6 +86,7 @@ public class AlignmentPanel extends JPanel implements ComponentListener {
 
 		nameXOff = 30;
 		nameYOff = 20;
+		glyphWidth = 12;
 		groupOff = nameYOff * (orderedKeys.size() + 1);
 
 		int allowedWidth = width - (4*nameXOff) - nameYOff;
@@ -77,6 +101,8 @@ public class AlignmentPanel extends JPanel implements ComponentListener {
 		len = align.length();
 		int seqLen = seqs.get(orderedKeys.get(0)).length();
 		height = ((seqLen/len)+1) * groupOff;
+
+		lineWidth = (width - nameXOff*4)/glyphWidth;
 
 		setPreferredSize( new Dimension(width,height) );
 	}
@@ -98,26 +124,45 @@ public class AlignmentPanel extends JPanel implements ComponentListener {
 		g2.setFont( defaultFont ); 
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-		int i = 0;
+		FontRenderContext frc = g2.getFontRenderContext();
 		Map<String,String> seqs = aligns.get(index).getSequences();
+		Map<String,String> cols = aligns.get(index).getColors();
+		int i = 0;
 		for ( String name : orderedKeys ) {
-			i++;	
+			i++;
+
 			String seq = seqs.get(name);
+			String colString = cols.get(name);
 			if ( seq == null )
 				continue;
-			int begin = 0;
-			int end = Math.min(len, seq.length()-1);
-			String sub = seq.substring(begin,end);
-			int y = 0;
-			while ( sub.length() > 0 ) {
-				g2.drawString(name,nameXOff,y*groupOff+nameYOff*i);
-				g2.drawString(sub,nameXOff*3,y*groupOff+nameYOff*i);
-				end = Math.min(end + len, seq.length()-1);
-				begin = Math.min(begin + len, end ); 
-				sub = seq.substring(begin,end);
-				y++;
+
+			GlyphVector gv = defaultFont.createGlyphVector(frc,seq);
+			
+			for ( int j = 0; j < gv.getNumGlyphs(); j++ ) {
+
+				int xLoc = nameXOff*3+(j%lineWidth)*glyphWidth;
+				int yLoc = (j/lineWidth)*groupOff+nameYOff*i;
+				int glyphYLoc = os*((j/lineWidth)*groupOff-nameYOff*i);
+
+				// set glyph position
+				Point2D pos = gv.getGlyphPosition(j); 
+				pos.setLocation(xLoc, glyphYLoc);
+				gv.setGlyphPosition(j,pos);
+
+				// draw the background rect 
+				g2.setPaint(colMap.get( colString.substring(j,j+1) ));
+				g2.fillRect(xLoc,yLoc-nameYOff+5,glyphWidth,nameYOff);
+
+				// draw the sequence name
+				if ( j%lineWidth == 0 ) {
+					g2.setPaint(Color.black);
+					g2.drawString(name,nameXOff,yLoc );
+				}
 			}
+
+			g2.setPaint(Color.black);
+			g2.drawGlyphVector(gv,0,0);
+
 		}
 	}
 
